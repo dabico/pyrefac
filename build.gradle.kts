e4272676f8ace8a311c2649caa740e5002419ffc
dabico <dabico@usi.ch>
2024-05-04 17:11:54 +0200

    Add PythonCore plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,16 @@ dependencies {
 }
 
 // https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+// https://plugins.jetbrains.com/docs/intellij/intellij-community-plugins-extension-point-list.html
 intellij {
     type.set("IC")
     version.set("2023.2.6")
-    plugins.set(listOfNotNull("Git4Idea"))
+    plugins.set(
+        listOfNotNull(
+            "Git4Idea",
+            "PythonCore:232.10300.40",
+        )
+    )
 }
 
 tasks {
