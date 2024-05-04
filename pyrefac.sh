ae63a84f27520bc18e99b249b3c23211c3b38a0b
dabico <dabico@usi.ch>
2024-05-04 12:05:13 +0200

    Add helper script

---
+++ pyrefac.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+if uname -s | grep -iq cygwin ; then
+    DIR=$(cygpath -w "$DIR")
+    PWD=$(cygpath -w "$PWD")
+fi
+
+"$DIR/gradlew" -p "$DIR" runPyRefac \
+  -Prepository="$1" \
+  -PfilePath="$2" \
+  -Prefactoring="$3" \
+  -Pparameters="$PWD/$4"
