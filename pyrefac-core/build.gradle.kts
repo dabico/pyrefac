e52d1abb2e7b4bec5d8dc5b518d23f1ef18d3a4c
dabico <dabico@usi.ch>
2024-05-08 18:57:26 +0200

    Add test dependencies

--- pyrefac-core/build.gradle.kts
+++ pyrefac-core/build.gradle.kts
@@ -1,2 +1,16 @@
 group = rootProject.group
 version = rootProject.version
+
+val junitVersion = "5.10.2"
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:${junitVersion}"))
+    testImplementation("org.junit.jupiter:junit-jupiter:${junitVersion}")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        jvmArgs = listOf("-Djdk.module.illegalAccess.silent=true")
+    }
+}
