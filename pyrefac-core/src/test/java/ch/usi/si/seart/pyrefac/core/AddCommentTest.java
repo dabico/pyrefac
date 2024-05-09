95b017719af1621af57cf539ebe9cab89db7756e
dabico <dabico@usi.ch>
2024-05-09 13:59:21 +0200

    Parametrize exceptional tests for constructors

--- pyrefac-core/src/test/java/ch/usi/si/seart/pyrefac/core/AddCommentTest.java
+++ pyrefac-core/src/test/java/ch/usi/si/seart/pyrefac/core/AddCommentTest.java
@@ -5,13 +5,26 @@ import com.jetbrains.python.PythonLanguage;
 import com.jetbrains.python.psi.PyFile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class AddCommentTest extends RefactoringTest {
 
-    @Test
-    public void testThrows() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new AddComment(null, null, "Random comment"));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new AddComment(null, "__init__", null));
+    private static Stream<Arguments> executables() {
+        return Stream.of(
+                Arguments.of((Executable) () -> new AddComment(null, null, "Random comment")),
+                Arguments.of((Executable) () -> new AddComment(null, "__init__", null))
+        );
+    }
+
+    @MethodSource("executables")
+    @ParameterizedTest(name = "Test {index}")
+    public void testThrows(Executable action) {
+        Assertions.assertThrows(IllegalArgumentException.class, action);
     }
 
     @Test
--- pyrefac-core/src/test/java/ch/usi/si/seart/pyrefac/core/RenameLiteralTest.java
+++ pyrefac-core/src/test/java/ch/usi/si/seart/pyrefac/core/RenameLiteralTest.java
@@ -1,14 +1,26 @@
 package ch.usi.si.seart.pyrefac.core;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class RenameLiteralTest extends RefactoringTest {
 
-    @Test
-    public void testThrows() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new RenameLiteral(null, null, "old_name", "new_name"));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new RenameLiteral(null, "__init__", null, "new_name"));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new RenameLiteral(null, "__init__", "old_name", null));
+    private static Stream<Arguments> executables() {
+        return Stream.of(
+                Arguments.of((Executable) () -> new RenameLiteral(null, null, "old_name", "new_name")),
+                Arguments.of((Executable) () -> new RenameLiteral(null, "__init__", null, "new_name")),
+                Arguments.of((Executable) () -> new RenameLiteral(null, "__init__", "old_name", null))
+        );
+    }
+
+    @MethodSource("executables")
+    @ParameterizedTest(name = "Test {index}")
+    public void testThrows(Executable action) {
+        Assertions.assertThrows(IllegalArgumentException.class, action);
     }
 }
