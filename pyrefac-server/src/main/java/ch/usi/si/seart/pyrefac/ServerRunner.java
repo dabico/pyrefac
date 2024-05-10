9c9e517cb388546d49a0f1c0f294f8ba4cd964d7
dabico <dabico@usi.ch>
2024-05-11 00:13:30 +0200

    Functioning server implementation

--- pyrefac-server/src/main/java/ch/usi/si/seart/pyrefac/ServerRunner.java
+++ pyrefac-server/src/main/java/ch/usi/si/seart/pyrefac/ServerRunner.java
@@ -1,10 +1,15 @@
 package ch.usi.si.seart.pyrefac;
 
+import ch.usi.si.seart.pyrefac.core.exception.NameAlreadyInUseException;
+import ch.usi.si.seart.pyrefac.core.exception.PsiNamedElementNotFoundException;
+import ch.usi.si.seart.pyrefac.core.jackson.PyRefacModule;
 import ch.usi.si.seart.pyrefac.server.RefactoringHandler;
 import ch.usi.si.seart.pyrefac.server.exception.NotRegularFileException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.intellij.openapi.application.ApplicationStarter;
 import io.javalin.Javalin;
-import org.eclipse.jgit.errors.TransportException;
+import io.javalin.json.JavalinJackson;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,13 +31,24 @@ public class ServerRunner implements ApplicationStarter {
                 .mapToInt(Integer::parseInt)
                 .findFirst()
                 .orElse(8080);
-        Javalin.create()
+        Javalin.create(config -> {
+                    config.showJavalinBanner = false;
+                    JavalinJackson mapper = new JavalinJackson();
+                    SimpleModule module = new PyRefacModule();
+                    mapper.updateMapper(delegate -> delegate.registerModule(module));
+                    config.jsonMapper(mapper);
+                })
                 .get("/", ctx -> ctx.status(200))
-                .get(RefactoringHandler.PATH, new RefactoringHandler())
+                .post(RefactoringHandler.PATH, new RefactoringHandler())
+                .exception(IllegalArgumentException.class, (ex, ctx) -> ctx.status(400))
+                .exception(JsonParseException.class, (ex, ctx) -> ctx.status(400))
                 .exception(NotRegularFileException.class, (ex, ctx) -> ctx.status(400))
-                .exception(TransportException.class, (ex, ctx) -> ctx.status(400))
+                .exception(NullPointerException.class, (ex, ctx) -> ctx.status(400))
                 .exception(FileNotFoundException.class, (ex, ctx) -> ctx.status(404))
+                .exception(PsiNamedElementNotFoundException.class, (ex, ctx) -> ctx.status(404))
+                .exception(NameAlreadyInUseException.class, (ex, ctx) -> ctx.status(409))
                 .exception(Exception.class, (ex, ctx) -> ctx.status(500))
+                .exception(UnsupportedOperationException.class, (ex, ctx) -> ctx.status(501))
                 .start(port);
     }
 }
--- pyrefac-server/src/main/java/ch/usi/si/seart/pyrefac/server/RefactoringHandler.java
+++ pyrefac-server/src/main/java/ch/usi/si/seart/pyrefac/server/RefactoringHandler.java
@@ -1,21 +1,37 @@
 package ch.usi.si.seart.pyrefac.server;
 
+import ch.usi.si.seart.pyrefac.core.Refactoring;
 import ch.usi.si.seart.pyrefac.server.exception.NotRegularFileException;
+import com.intellij.lang.Language;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.psi.PsiFileFactory;
+import com.jetbrains.python.PythonLanguage;
+import com.jetbrains.python.psi.PyFile;
+import git4idea.commands.Git;
+import git4idea.commands.GitCommand;
+import git4idea.commands.GitCommandResult;
+import git4idea.commands.GitLineHandler;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jgit.api.Git;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class RefactoringHandler implements Handler {
 
     public static final String PATH = "/{owner}/{name}/*";
+
+    private final Git git = Git.getInstance();
+    ProjectManager projectManager = ProjectManager.getInstance();
 
     @Override
     public void handle(@NotNull Context ctx) throws Exception {
@@ -25,21 +41,37 @@ public final class RefactoringHandler implements Handler {
         String url = String.format("https://github.com/%s/%s.git", owner, name);
         String path = ctx.path();
         String file = StringUtils.removeStart(path, prefix);
-        try (
-                TemporaryDirectory root = TemporaryDirectory.create();
-                Git ignored = Git.cloneRepository()
-                        .setDirectory(root.path().toFile())
-                        .setURI(url)
-                        .setDepth(1)
-                        .call()
-        ) {
+        Refactoring refactoring = ctx.bodyAsClass(Refactoring.class);
+        try (TemporaryDirectory root = TemporaryDirectory.create()) {
+            GitLineHandler handler = new GitLineHandler(null, root.path.getParent().toFile(), GitCommand.CLONE);
+            handler.addParameters(url, root.path.toString(), "--depth=1");
+            GitCommandResult result = git.runCommand(handler);
+            if (!result.success()) throw new IOException(result.getErrorOutputAsJoinedString());
+
             Path absolute = root.path().resolve(file);
             if (!Files.exists(absolute))
                 throw new FileNotFoundException(absolute.toString());
             if (!Files.isRegularFile(absolute))
                 throw new NotRegularFileException(absolute.toString());
-            String contents = Files.readString(absolute);
-            ctx.html("<pre><code>" + contents + "</code></pre>");
+            String original = Files.readString(absolute);
+
+            StringBuilder builder = new StringBuilder();
+            ApplicationManager.getApplication().invokeAndWait(() -> {
+                Project project = projectManager.getDefaultProject();
+                PsiFileFactory fileFactory = PsiFileFactory.getInstance(project);
+                Language language = PythonLanguage.getInstance();
+                PyFile pyFile = (PyFile) fileFactory.createFileFromText(".py", language, original, false, true);
+                refactoring.perform(pyFile);
+                builder.append(pyFile.getText());
+            });
+            FileUtils.writeStringToFile(absolute.toFile(), builder.toString(), StandardCharsets.UTF_8);
+
+            handler = new GitLineHandler(null, root.path.toFile(), GitCommand.DIFF);
+            result = git.runCommand(handler);
+            if (!result.success()) throw new IOException(result.getErrorOutputAsJoinedString());
+            String diff = result.getOutputAsJoinedString();
+            ctx.status(diff.isEmpty() ? 204 : 200);
+            ctx.result(diff);
         }
     }
 
